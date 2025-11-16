3b4cdf9e593e0a3aba355ae789235db291208325
vedantkr <110099368+vedantkumar18@users.noreply.github.com>
2025-11-17 03:11:37 +0530

    Create script.js with JavaScript coding practices
    Add JavaScript practice exercises and comments

---
+++ learn-js/script.js
@@ -0,0 +1,209 @@
+/* 
+console.log (`apna course`);
+age = 24;
+console.log(age);
+x = null;
+console.log(x);
+y = undefined;
+console.log(y);
+let exampleLet= `variable cannot be re-declared but can be updated. its block scoped`;
+const exampleConst= `is a variable that cannot be re-declared or updated. it is also block scoped.`;
+var exampleVar = `variable can be re-declared or updated . its global so works everywhere`;
+*/
+//57 mins done in coding pracs
+
+/* const studentDetails = {
+    nameOfStudent: `kevin`,
+    ageOfStudent: 21,
+    marksOfStudent: 93,
+    ispass: true ,
+};
+console.log(studentDetails.nameOfStudent);
+console.log(studentDetails);
+*/
+
+// lesson 1 done
+//prac 1
+/* const product = {
+    namePen: `Parker Jotter Standard CT Ball Pen (black)`,
+    review: 7002,
+    price: 270,
+    offer: 5,
+};
+console.log(product);
+*/
+// prac 2 
+/* const profile = {
+    name: `Kevin`,
+    username: `Kevin heart`,
+    userTag: `@Kevin_heart`,
+    posts: 100,
+    followers: 10,
+    following: 3,
+    isFollow: true, // button
+    title: `Entreprenuer`,
+    bio: `Harvard | student`,
+}
+console.log(profile);
+*/
+
+// lesson 1 done
+// lesson 2 - 20 mins done 
+
+//let a = 5;
+//let b = 3;
+
+/* + ,- ,* ,/ ,++a ,a++ ,% ,**
+ console.log(a + b); // add
+ console.log(a - b); // substract
+ console.log(a * b); // multiply
+ console.log(a / b); // divide
+ console.log(a++); // same as below
+ console.log(++a);// increment and decrement will be minus
+ console.log(a % b); // remainder
+ console.log(a ** 3); // exponents */
+
+/* assignment operators
+ +=, -=, /=, *=, **= 
+ a += 4 // a = a + 4
+ console.log(a); */
+
+/* comparison operators
+ ==, ===, !=, !==, >, >=, <, <= */
+
+/* console.log(a == b); // false
+ console.log(a != b); // true
+ console.log( a === b); // false
+ console.log( a !== b); // true 
+ the greater than / equal to */
+
+/*console.log(a > b); // true 
+ console.log(a < b); // false
+ console.log(a >= b); // true
+ console.log(a <= b);// false */
+
+// logical operators 
+// && || ! 
+
+// logical &&
+//let cond1 = a > b; //true
+//let cond2 = a  === 5; // true 
+//console.log(cond1 && cond2);
+//console.log(cond1);
+//console.log(cond2);
+
+//logical || is used if either 1 cond has or can be true for the process to contniue
+
+//let color;
+//if(mode === `dark-mode`){
+//    color = `black`;
+//}
+
+/*let license;
+if(age >= 18){
+    license = `given`;
+} else {
+    license = `you are too young`;
+}
+console.log(license);*/
+
+// lesson 2 - done till 41 min and 45 sec, will have to start from the mode example.s
+
+/*
+let mode = `light`;
+let color;
+if (mode === `dark`){
+    color =`dark`;
+} else if (mode === `light`) {
+    color = `light`;
+} else {
+    color = null;
+}
+console.log(color); */
+
+/* let num = 9;
+if (num%2 === 0) {
+    console.log(`even`);
+}
+else {
+    console.log(`odd`);
+} */
+
+
+/* let age = 10;
+if(age < 18){
+    console.log(`junior`);
+} else if(age > 60){
+    console.log(`senior`);    
+} else {
+    console.log(`middle`);
+}
+console.log(age); */
+
+// ternary operator
+
+/* let age = 10;
+let msg = age > 18 ? `adult` : `u are still a child`;
+console.log(msg); */
+
+// q1
+/*
+let userInput = prompt(`Enter a number`);
+console.log(userInput);
+
+if (userInput % 5 === 0) {
+    console.log(userInput + ` is a multiple of 5.`);
+} else {
+    console.log(userInput + ` is not a multiple of 5`);
+} */
+
+/* let studentMarks = prompt(`please input the exact marks that you've scored`);
+if (studentMarks >= 80 && studentMarks <= 100) {
+    console.log(`A`);
+} else if (studentMarks >= 70 && studentMarks <= 79) {
+    console.log(`B`);
+} else if (studentMarks >= 60 && studentMarks <= 69) {
+    console.log(`C`);
+} else if (studentMarks >= 50 && studentMarks <= 59) {
+    console.log(`D`);
+} else if (studentMarks >= 0 && studentMarks <= 49) {
+    console.log (`F`);
+}
+console.log(studentMarks); */
+
+// lesson - 2 is done.
+
+// lesson - 3 
+
+// for loop 
+
+/* for (let i = 1; i <= 10; i++){
+    console.log(`apna college`);
+} */
+
+// calculate sum of 1 to 5
+
+/* let sum = 0
+ let n = prompt(`please input a number`);
+for(let i = 1; i <= n; i++){
+    sum = sum + i;
+}
+console.log(sum); */
+
+// while loop
+
+/* let i = 1;
+while( i <= 5){
+ console.log(i);
+ i++;
+} */
+
+ // do while loop
+
+/* let i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i <= 5); */
+
+// lesson - 3 done till 26:40 
